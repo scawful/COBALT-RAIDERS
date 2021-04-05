80314ce38a9c6f3d58675aa91c178639d0b40f41
Justin Scofield <justin.scofield@outlook.com>
2021-04-05 11:43:57 -0500

    Created Bullet class header

---
+++ source/Bullet/Bullet.hpp
@@ -0,0 +1,39 @@
+//  Bullet Class Header
+
+#ifndef Bullet_hpp
+#define Bullet_hpp
+
+#include "../State/State.hpp"
+#include "../Sprite/Sprite.hpp"
+
+class Bullet
+{
+private:
+    Sprite sprite;
+    Vector2f velocity;
+
+public:
+    Bullet( SDL_Texture *texture, Vector2f velocity, Vector2f position, SDL_Rect rect )
+    {
+        this->velocity = velocity;
+        sprite.setTexture( *texture );
+        sprite.setTextureRect( rect );
+        sprite.setPosition( position );
+    }
+
+    void update( double timeElapsed )
+    {
+        Vector2f newPos = { sprite.getPosition().x + timeElapsed * velocity.x,
+                            sprite.getPosition().y + timeElapsed * velocity.y };
+                    
+        sprite.setPosition( newPos );
+    }
+
+    Sprite* getSprite()
+    {
+        return &sprite;
+    }
+
+};
+
+#endif
